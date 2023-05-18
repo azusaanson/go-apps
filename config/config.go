8fe47c3e8185c9f764eac8115990f1f6e6ffb358
azusaanson <yth041512@gmail.com>
2023-05-19 02:50:00 +0900

    add config

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver     string `mapstructure:"DB_DRIVER"`
+	DBUser       string `mapstructure:"DB_USER"`
+	DBPassword   string `mapstructure:"DB_PASSWORD"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBPort       string `mapstructure:"DB_PORT"`
+	DBName       string `mapstructure:"DB_NAME"`
+	MigrationURL string `mapstructure:"MIGRATION_URL"`
+
+	GRPCServer string `mapstructure:"GRPC_SERVER"`
+
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
